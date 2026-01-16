ec3c6941a0753d23609869ddbeaca7c9915ae62a
xianrendesu-max <xianrendesu@gmail.com>
2026-01-17 06:48:01 +0900

    Update cache.py

--- cache.py
+++ cache.py
@@ -1,24 +1,22 @@
 import time
-from threading import Lock
 
-class TTLCache:
+class SimpleCache:
     def __init__(self):
         self.store = {}
-        self.lock = Lock()
 
     def get(self, key):
-        with self.lock:
-            item = self.store.get(key)
-            if not item:
-                return None
-            value, expire = item
-            if expire < time.time():
-                del self.store[key]
-                return None
-            return value
+        item = self.store.get(key)
+        if not item:
+            return None
+        value, expire = item
+        if expire and expire < time.time():
+            del self.store[key]
+            return None
+        return value
 
     def set(self, key, value, ttl=60):
-        with self.lock:
-            self.store[key] = (value, time.time() + ttl)
+        expire = time.time() + ttl if ttl else None
+        self.store[key] = (value, expire)
 
-cache = TTLCache()
+
+cache = SimpleCache()
